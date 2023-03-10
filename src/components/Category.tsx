0190f27f2b221dcb09c4dfe3125072891001bbb0
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-03-11 01:39:07 +0300

    created styled component based on NavLink with implementing of active logic

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,27 +1,26 @@
 import { FaPizzaSlice, FaHamburger } from 'react-icons/fa'
 import { GiNoodles, GiChopsticks } from 'react-icons/gi'
-import { NavLink } from 'react-router-dom'
-import { List } from '../utils/styled-components'
+import { List, SLink } from '../utils/styled-components'
 
 const Category: React.FC = () => {
   return (
     <List>
-      <NavLink to={'/cuisine/Italian'}>
+      <SLink to={'/cuisine/Italian'}>
         <FaPizzaSlice />
-        <h4>Итальянская</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/American'}>
+        <h4>Италия</h4>
+      </SLink>
+      <SLink to={'/cuisine/American'}>
         <FaHamburger />
-        <h4>Американская</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/Thai'}>
+        <h4>Америка</h4>
+      </SLink>
+      <SLink to={'/cuisine/Thai'}>
         <GiNoodles />
-        <h4>Тайская</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/Japanese'}>
+        <h4>Тайланд</h4>
+      </SLink>
+      <SLink to={'/cuisine/Japanese'}>
         <GiChopsticks />
-        <h4>Японская</h4>
-      </NavLink>
+        <h4>Япония</h4>
+      </SLink>
     </List>
   )
 }
--- src/utils/styled-components/index.ts
+++ src/utils/styled-components/index.ts
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
@@ -69,5 +70,43 @@ export const GridCard = styled.div`
   h4 {
     text-align: center;
     padding: 1rem;
+  }
+`
+
+export const SLink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 2rem;
+  text-decoration: none;
+  background: linear-gradient(35deg, #494949, #313131);
+  width: 6rem;
+  height: 6rem;
+  cursor: pointer;
+  transform: scale(0.8);
+
+  h4 {
+    padding-top: 10px;
+    color: white;
+    font-size: 0.8rem;
+  }
+
+  svg {
+    color: white;
+    font-size: 1.5rem;
+  }
+
+  &.active {
+    background: linear-gradient(to right, #f27121, #e94057);
+
+    svg {
+      color: white;
+    }
+
+    h4 {
+      color: white;
+    }
   }
 `
